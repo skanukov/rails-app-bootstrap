d88cac3afb03184a76482598171d1cc1a9117721
Sergey Kanukov <s.kanukov@gmail.com>
2015-12-25 15:25:20 +0300

    add Frontend::Home controller with index action and set root route to it

---
+++ app/controllers/frontend/home_controller.rb
@@ -0,0 +1,6 @@
+module Frontend
+  class HomeController < ApplicationController
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  scope module: 'frontend' do
+    root 'home#index'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
